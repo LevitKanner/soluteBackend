dbb125909d659521532ee3ae84cff44dc242a024
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-04 12:36:26 +0000

    Creates an errors object to holder all possible errors

---
+++ broker/utils/errors.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoError: 'MongoError',
+    passwordInvalid: 'Invalid password',
+    emailInvalid: 'Invalid email',
+    duplicateEmail: 'Email already exist',
+    userNotExist: 'User does not exist',
+    companyNotExist: 'Company does not exist'
+}
