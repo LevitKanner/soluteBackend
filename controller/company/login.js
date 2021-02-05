6382e6653fe30aef5925b1c6fabc5cdb49231f4c
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-05 19:18:14 +0000

    Adds data validation to company login

--- controller/company/login.js
+++ controller/company/login.js
@@ -1,6 +1,7 @@
 module.exports = async (req, res, next) => {
-    try{
-        const company = await _broker.companyService.login(req.body)
+    try {
+        const result = await _broker.validation.company.login.validateAsync(req.body)
+        const company = await _broker.companyService.login(result)
         res.send(company)
     }catch (e) {
        _broker.utils.errorHandler(e, next)
