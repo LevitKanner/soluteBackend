653fbe7a53d8da7c18cb2573accbf93be2cc2356
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-05 14:44:34 +0000

    Changes company telephone prop to phone

--- broker/db/models/Company.js
+++ broker/db/models/Company.js
@@ -4,19 +4,19 @@ const { VacancySchema } = require('./Vacancy')
 const CompanySchema = new mongoose.Schema({
     name: {
         type: String,
-        require: true
+        required: true
     },
     email: {
         type: String,
         lowercase: true,
         unique: true,
-        require: true
+        required: true
     },
     password: {
         type: String,
         required: true
     },
-    telephone: {
+    phone: {
         type: String,
         required: true
     },
