e9224efc234bf92c45f2120444ca12b8ed8b32ca
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-05 19:20:12 +0000

    Adds login route to company routes

--- routes/company.js
+++ routes/company.js
@@ -7,9 +7,12 @@ const deleteCompany = require('../controller/company/delete')
 const getAllCompanies = require('../controller/company/allCompanies')
 const getCompanyDetails = require('../controller/company/company-details')
 const updateCompany = require('../controller/company/updateCompany')
+const login = require('../controller/company/login')
 
 
 router.get('/', getAllCompanies)
+
+router.post('/login', login)
 
 router.post('/register', registerCompany)
 
