fd80c0bdf277dbed1b7437a246c94a1f938cb4bf
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-05 19:18:59 +0000

    Adds data validation to user login

--- controller/user/login.js
+++ controller/user/login.js
@@ -1,7 +1,8 @@
 module.exports = async (req, res, next) => {
     try {
-        const result = await _broker.userService.login(req.body)
-        res.send(result)
+        const result = await _broker.validation.user.login.validateAsync(req.body)
+        const user = await _broker.userService.login(result)
+        res.send(user)
     } catch (e) {
         _broker.utils.errorHandler(e, next)
     }
