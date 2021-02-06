15dc35205e0507dde31102119f58bb326b500618
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-06 14:14:37 +0000

    Improves error handling

--- broker/utils/errorHandler.js
+++ broker/utils/errorHandler.js
@@ -3,13 +3,13 @@ const createError = require('http-errors')
 const error = require('./errors')
 
 const errorHandler = (err, next) => {
-    if (err.message === error.mongoError )  return next(createError(404, err.message))
+    if (err.name === 'MongoError' )  return next(createError.InternalServerError(err.message))
 
-    if (err.name === 'ValidationError') return next(createError(422, err.message))
+    if (err.name === 'ValidationError') return next(createError.BadRequest(err.message))
 
     if (err.name === 'ConflictError') return next(createError(400, err.message))
 
-    if (err.message === error.passwordInvalid) return next(createError(422, err.message))
+    if (err.name === 'UnauthorizedError') return next(createError.Unauthorized(err.message))
 
     if (err.message === error.emailInvalid) return next(createError(400, err.message))
 
