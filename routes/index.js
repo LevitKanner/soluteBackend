143ebd9d929979adf762e23ad91a5172259c0145
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-07 15:24:35 +0000

    protects the index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const verifyToken = require('../middlewares/verifyToken')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', verifyToken, (req, res, next) => {
+  res.send({ title: 'Express', payload: req.payload});
 });
 
 module.exports = router;
