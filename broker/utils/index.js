1ffc2ba5dd71157cd5ec478a1d89562cfda2bd7d
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-08 10:28:16 +0000

    Edits verifying and generating refresh token functions to add redis

--- broker/utils/index.js
+++ broker/utils/index.js
@@ -5,6 +5,8 @@ const connectDB = require('./database')
 const errorHandler = require('./errorHandler')
 const errors = require('./errors')
 const { hashPassword, validatePasswordHash } = require('./password')
+const redisClient = require('./redis')
+
 
 const generateAccessToken = (id) => {
     return new Promise((resolve, reject) => {
@@ -22,6 +24,7 @@ const generateAccessToken = (id) => {
     })
 }
 
+
 const generateRefreshToken = (id) => {
     return new Promise((resolve, reject) => {
         const payload = {}
@@ -31,9 +34,14 @@ const generateRefreshToken = (id) => {
             'issuer': 'solute.com',
             'audience': id
         }
-        JWT.sign(payload, secret, options, (err, payload) => {
-            if (err) return reject(createError.Unauthorized()) 
-            return resolve(payload)
+        JWT.sign(payload, secret, options, (err, token) => {
+            if (err) return reject(createError.Unauthorized())
+
+            redisClient.SET(id, token, (err, reply) => {
+                if (err) return reject(createError.InternalServerError())
+                return resolve(token)
+            })
+            
         })
     })
 }
@@ -41,7 +49,16 @@ const generateRefreshToken = (id) => {
 const verifyRefreshToken = (token) => {
     return new Promise((resolve, reject) => {
         JWT.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, payload) => {
-           return err ? reject(createError.Unauthorized()) : resolve(payload.aud)
+            if (err) return reject(createError.Unauthorized())
+            
+            const id  = payload.aud
+            
+            redisClient.GET(id, (err, reply) => {
+                if (err) return reject(createError.InternalServerError())
+                if (reply !== token) return reject(createError.Unauthorized())
+                resolve(id)
+            }) 
+           
         })
     })
 }
