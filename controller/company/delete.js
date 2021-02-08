7ae63b9f26b8da0449cd152c1fdae60e6221ecf0
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-08 10:26:46 +0000

    Adds proper error handling to company delete route

--- controller/company/delete.js
+++ controller/company/delete.js
@@ -1,10 +1,12 @@
+const createError = require('http-errors')
+
 module.exports = async (req, res, next) => {
     const {id} = req.params;
     try {
-        const company = await _broker.companyService.deleteCompany({id})
+        const company = await _broker.companyService.deleteCompany({ id })
+        if(!company) return next(createError.NotFound('company not found'))
         res.send(company)
     } catch (e) {
         _broker.utils.errorHandler(e, next)
     }
-
 }
