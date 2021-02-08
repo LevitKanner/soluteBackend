03f1d114a0b57ec2b95b2d4a0ac4c15143169b02
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-08 10:25:59 +0000

    Adds redis for storing access tokens

---
+++ broker/utils/redis.js
@@ -0,0 +1,28 @@
+const redis = require('redis')
+
+const client = redis.createClient({
+    port: 6379,
+    host: '127.0.0.1'
+})
+
+client.on('connected', () => {
+    console.log('Redis client connected')
+})
+
+client.on('ready', () => {
+    console.log('Redis client connected and ready...')
+})
+
+client.on('error', (err) => {
+    console.log(err.message)
+})
+
+client.on('end', () => {
+    console.log('Closing redis')
+})
+
+process.on('SIGINT', () => {
+    client.end()
+})
+
+module.exports = client
