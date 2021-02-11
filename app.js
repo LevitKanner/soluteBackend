69662482304287fc5c1f6135a6f66d688e8c3207
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-11 12:48:14 +0000

    Adds swaggerUI and compression middlewares

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const createError = require('http-errors')
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const cors = require('cors')
 require('dotenv').config();
+
+const compression = require('./middlewares/compression')
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -14,10 +20,32 @@ global._broker = require('./broker')
 
 _broker.utils.connectDB();
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: "Solute API",
+            version: "1.0.0",
+            description: "Backend Application for Solute"
+        },
+        servers: [
+            {
+                url: "http://localhost:7000"
+            }
+        ]
+    },
+    apis: ["./routes/*.js"]
+}
+
+const specs = swaggerJsDoc(options);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(specs))
+app.use(cors())
+app.use(compression)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
