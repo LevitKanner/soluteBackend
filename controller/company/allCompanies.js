79db17b8141ddc7147fd1663ead8feab74114491
Levit Osei Kanner <lkanner21@gmail.com>
2021-02-14 09:40:07 +0000

    Adds pagination to allCompanies and allUsers controllers

--- controller/company/allCompanies.js
+++ controller/company/allCompanies.js
@@ -1,7 +1,15 @@
 module.exports = async (req, res, next) => {
+    const { page = 1, size = 2 } = req.query;
+
+    const limit = parseInt(size)
+    const skip = (page - 1) * limit;
     try {
-        const companies = await _broker.companyService.allCompanies()
-        res.send(companies)
+        const companies = await _broker.companyService.allCompanies(skip,limit)
+        res.send({
+            page: parseInt(page),
+            count: limit,
+            companies
+        })
     } catch (e) {
         _broker.errorHandler(e, next)
     }
--- controller/user/allUsers.js
+++ controller/user/allUsers.js
@@ -1,7 +1,15 @@
 module.exports = async (req, res, next) => {
+    const { page = 1, size = 2 } = req.query;
+
+    const limit = parseInt(size)
+    const skip = (page - 1) * limit;
     try {
-        const users = await _broker.userService.allUsers()
-        res.send(users)
+        const users = await _broker.userService.allUsers(skip, limit)
+        res.send({
+            page: parseInt(page),
+            count: limit,
+            users
+        })
     }catch(e) {
         _broker.utils.errorHandler(e, next)
     }
